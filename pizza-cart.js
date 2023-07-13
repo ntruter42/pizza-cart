fa3683dda966de2463a7aa971c67fd272077c579
ntruter42 <ntruter42@gmail.com>
2023-07-13 12:33:06 +0200

    Created payment and checkout functions

--- pizza-cart.js
+++ pizza-cart.js
@@ -1,11 +1,16 @@
 function pizzaCart() {
 	return {
+		message: ``,
+		checkout: false,
 		payment: 0,
+		total: 0,
+		msgTimeout: null,
+		msgTime: 3000,
 
 		price: {
-			'small': 50,
-			'medium': 70,
-			'large': 80
+			'small': 49.99,
+			'medium': 69.99,
+			'large': 99.99
 		},
 
 		count: {
@@ -20,21 +25,28 @@ function pizzaCart() {
 			'large': 0
 		},
 
-		addPizza(size) {
-			if (this.count[size] >= 1) {
-				this.count[size] += 1;
-				this.updateCosts();
-			} else {
-				// show message
+		clickCheckout() {
+			this.checkout = false;
+			for (const pizza in this.count) {
+				if (this.count[pizza] > 0) {
+					this.checkout = true;
+				}
+			}
+			if (this.checkout === false) {
+				this.displayMessage(`No pizzas in cart`);
 			}
 		},
 
-		removePizza(size) {
-			if (this.count[size] > 0) {
-				this.count[size] -= 1;
+		makePayment() {
+			if (this.payment >= this.total) {
+				for (const pizza in this.count) {
+					this.count[pizza] = 0;
+				}
+				this.checkout = false;
 				this.updateCosts();
+				this.displayMessage(`Thank you for your support. Enjoy!`);
 			} else {
-				// show message
+				this.displayMessage(`Not enough money`);
 			}
 		},
 
@@ -43,7 +55,26 @@ function pizzaCart() {
 				this.count[size] = 1;
 				this.updateCosts();
 			} else {
-				// show message
+				this.displayMessage(`${size.charAt(0).toUpperCase() + size.slice(1)} pizza is already in cart`);
+			}
+		},
+
+		addPizza(size) {
+			this.checkout = false;
+			if (this.count[size] >= 1) {
+				this.count[size] += 1;
+				this.updateCosts();
+			} else {
+				this.displayMessage(`First add a ${size} pizza to cart`);
+			}
+		},
+
+		removePizza(size) {
+			if (this.count[size] > 0) {
+				this.count[size] -= 1;
+				this.updateCosts();
+			} else {
+				this.displayMessage(`No pizzas to subtract`);
 			}
 		},
 
@@ -51,6 +82,15 @@ function pizzaCart() {
 			this.cost.small = this.count.small * this.price.small;
 			this.cost.medium = this.count.medium * this.price.medium;
 			this.cost.large = this.count.large * this.price.large;
+			this.total = this.cost.small + this.cost.medium + this.cost.large;
 		},
+
+		displayMessage(message) {
+			clearTimeout(this.msgTimeout);
+			this.message = message;
+			this.msgTimeout = setTimeout(() => {
+				this.message = ``;
+			}, this.msgTime);
+		}
 	}
 }
